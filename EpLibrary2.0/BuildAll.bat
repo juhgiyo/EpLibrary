40c396d89113b3b0bc47e56855dfd4b32c3aea7c
Woong Gyu La <juhgiyo@gmail.com>
2012-11-13 20:22:40 +0900

    VS2012 Support updated

--- EpLibrary2.0/BuildAll.bat
+++ EpLibrary2.0/BuildAll.bat
@@ -24,7 +24,7 @@ if not exist "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.ex
 
 :VS10Compile
 
-if not exist "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" goto :Done
+if not exist "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" goto :VS11Compile
 
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpLibrary100.sln /build "Debug" /project "EpLibrary\EpLibrary100.vcxproj" /projectconfig "Debug"
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpLibrary100.sln /build "Debug DLL" /project "EpLibrary\EpLibrary100.vcxproj" /projectconfig "Debug DLL"
@@ -34,5 +34,19 @@ if not exist "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv.e
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpLibrary100.sln /build "Release DLL" /project "EpLibrary\EpLibrary100.vcxproj" /projectconfig "Release DLL"
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpLibrary100.sln /build "Release DLL Unicode" /project "EpLibrary\EpLibrary100.vcxproj" /projectconfig "Release DLL Unicode"
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpLibrary100.sln /build "Release Unicode" /project "EpLibrary\EpLibrary100.vcxproj" /projectconfig "Release Unicode"
+
+:VS11Compile
+
+if not exist "C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" goto :Done
+
+"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\devenv" EpLibrary110.sln /build "Debug" /project "EpLibrary\EpLibrary110.vcxproj" /projectconfig "Debug"
+"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\devenv" EpLibrary110.sln /build "Debug DLL" /project "EpLibrary\EpLibrary110.vcxproj" /projectconfig "Debug DLL"
+"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\devenv" EpLibrary110.sln /build "Debug DLL Unicode" /project "EpLibrary\EpLibrary110.vcxproj" /projectconfig "Debug DLL Unicode"
+"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\devenv" EpLibrary110.sln /build "Debug Unicode" /project "EpLibrary\EpLibrary110.vcxproj" /projectconfig "Debug Unicode"
+"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\devenv" EpLibrary110.sln /build "Release" /project "EpLibrary\EpLibrary110.vcxproj" /projectconfig "Release"
+"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\devenv" EpLibrary110.sln /build "Release DLL" /project "EpLibrary\EpLibrary110.vcxproj" /projectconfig "Release DLL"
+"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\devenv" EpLibrary110.sln /build "Release DLL Unicode" /project "EpLibrary\EpLibrary110.vcxproj" /projectconfig "Release DLL Unicode"
+"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\devenv" EpLibrary110.sln /build "Release Unicode" /project "EpLibrary\EpLibrary110.vcxproj" /projectconfig "Release Unicode"
+
 
 :Done
