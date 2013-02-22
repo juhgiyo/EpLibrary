622ce89794bb38e2ecf4533cebc03d050f54ff3b
Woong Gyu La <juhgiyo@gmail.com>
2013-02-22 13:05:57 +0900

    Event Extension added

---
+++ EpLibrary2.0/EpLibrary/Headers/epEventEx.h
@@ -0,0 +1,175 @@
+/*! 
+@file epEventEx.h
+@author Woong Gyu La a.k.a Chris. <juhgiyo@gmail.com>
+		<http://github.com/juhgiyo/eplibrary>
+@date April 16, 2011
+@brief EventEx Interface
+@version 2.0
+
+@section LICENSE
+
+Copyright (C) 2012  Woong Gyu La <juhgiyo@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@section DESCRIPTION
+
+An Interface for EventEx Class.
+
+*/
+#ifndef __EP_EVENT_EX_H__
+#define __EP_EVENT_EX_H__
+#include "epLib.h"
+#include "epSystem.h"
+#include "epBaseLock.h"
+
+namespace epl
+{
+
+	/*! 
+	@class EventEx epEventEx.h
+	@brief A class that handles the event functionality.
+	*/
+	class EP_LIBRARY EventEx :public BaseLock
+	{
+	public:
+		/*!
+		Default Constructor
+
+		Initializes the lock.
+		@remark eventName must be supplied if the object will be used across process boundaries.
+		@param[in] eventName name of the event to distinguish
+		@param[in] lpsaAttributes the security attribute
+		*/
+		EventEx(const TCHAR *eventName=NULL, LPSECURITY_ATTRIBUTES lpsaAttributes = NULL);
+
+		/*!
+		Default Constructor
+
+		Initializes the lock.
+		@param[in] isInitialRaised flag to raise the event on creation
+		@param[in] isManualReset flag to whether event is reset manually
+		@param[in] eventName name of the event to distinguish
+		@param[in] lpsaAttributes the security attribute
+		@remark eventName must be supplied if the object will be used across process boundaries.
+		@remark if isInitialRaised is TRUE then the event is raised on creation.
+		*/
+		EventEx(bool isInitialRaised,bool isManualReset,const TCHAR *eventName=NULL , LPSECURITY_ATTRIBUTES lpsaAttributes=NULL);
+
+		/*!
+		Default Copy Constructor
+
+		Initializes the EventEx
+		@param[in] b the second object
+		*/
+		EventEx(const EventEx& b);
+
+		/*!
+		Default Destructor
+
+		Deletes the lock
+		*/
+		virtual ~EventEx();
+
+		/*!
+		Assignment operator overloading
+		@param[in] b the second object
+		@return the new copied object
+		*/
+		EventEx & operator=(const EventEx&b);
+
+
+		/*!
+		Locks the Critical Section
+		@return true if locked, false otherwise
+		*/
+		virtual bool Lock();
+
+		/*!
+		Try to Lock the Critical Section
+
+		If other process is already in the Critical Section, it just returns false and continue, otherwise obtain the Critical Section.
+		@return true if the lock is succeeded, otherwise false.
+		*/
+		virtual long TryLock();
+
+		/*!
+		Locks the Critical Section
+
+		if other process is already in the Critical Section,
+		and if it fails to lock in given time, it returns false, otherwise lock and return true.
+		@param[in] dwMilliSecond the wait time.
+		@return true if the lock is succeeded, otherwise false.
+		*/
+		virtual long TryLockFor(const unsigned int dwMilliSecond);
+
+		/*!
+		Leave the Critical Section
+
+		The Lock and Unlock has to be matched for each Critical Section.
+		*/
+		virtual void Unlock();
+
+		/*!
+		Reset the event raised
+		@return true if succeeded otherwise false
+		@remark if event is not raised then no effect
+		*/
+		bool ResetEvent();
+
+		/*!
+		Set the event to be raised
+		@return true if succeeded otherwise false
+		@remark if event is already raised then no effect
+		@remark this function is same as unlock
+		*/
+		bool SetEvent();
+
+		/*!
+		Returns the flag whether this event is resetting manually.
+		@return true if the event is resetting manually, otherwise false.
+		*/
+		bool IsManualReset() const;
+
+		/*!
+		Wait for the event raised for given time
+		@param[in] dwMilliSecond the wait time.
+		@return true if the wait is succeeded, otherwise false.
+		*/
+		bool WaitForEvent(const unsigned int dwMilliSecond=WAITTIME_INIFINITE);
+
+	private:
+
+		/// EventEx
+		HANDLE m_event;
+		/// Creation Security Info
+		LPSECURITY_ATTRIBUTES m_lpsaAttributes;
+		/// Flag for whether the event is raised on creation
+		bool m_isInitialRaised;
+		/// Flag for whether the event is resetting manually.
+		bool m_isManualReset;
+		/// Event Name
+		EpTString m_name;
+
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+		/// thread ID that currently holding this event
+		unsigned long m_threadID;
+		/// EventEx Debug
+		HANDLE m_eventDebug;
+#endif //defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	};
+
+}
+
+#endif //__EP_EVENT_EX_H__
---
+++ EpLibrary2.0/EpLibrary/Sources/epEventEx.cpp
@@ -0,0 +1,228 @@
+/*! 
+EventEx for the EpLibrary
+Copyright (C) 2012  Woong Gyu La <juhgiyo@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "epEventEx.h"
+#include "epSystem.h"
+#include "epException.h"
+using namespace epl;
+
+
+EventEx::EventEx(const TCHAR *eventName, LPSECURITY_ATTRIBUTES lpsaAttributes) :BaseLock()
+{
+	m_isManualReset=false;
+	m_isInitialRaised=true;
+	m_lpsaAttributes=NULL;
+	if(lpsaAttributes)
+	{
+		m_lpsaAttributes=EP_NEW SECURITY_ATTRIBUTES();
+		*m_lpsaAttributes=*lpsaAttributes;
+	}
+	m_name=eventName;
+	m_event=CreateEvent(m_lpsaAttributes,(BOOL)m_isManualReset,(BOOL)m_isInitialRaised,eventName);
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	m_eventDebug=CreateEvent(m_lpsaAttributes,FALSE,TRUE,NULL);
+	m_threadID=0;
+#endif //defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+}
+
+EventEx::EventEx(bool isInitialRaised, bool isManualReset,const TCHAR *eventName , LPSECURITY_ATTRIBUTES lpsaAttributes) :BaseLock()
+{
+	m_isManualReset=isManualReset;
+	m_isInitialRaised=isInitialRaised;
+	m_lpsaAttributes=NULL;
+	if(lpsaAttributes)
+	{
+		m_lpsaAttributes=EP_NEW SECURITY_ATTRIBUTES();
+		*m_lpsaAttributes=*lpsaAttributes;
+	}
+
+	m_name=eventName;
+	m_event=CreateEvent(m_lpsaAttributes,(BOOL)m_isManualReset,(BOOL)m_isInitialRaised,eventName);
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	m_eventDebug=CreateEvent(m_lpsaAttributes,FALSE,TRUE,NULL);
+	m_threadID=0;
+	if(m_isInitialRaised)
+		m_threadID=GetCurrentThreadId();
+#endif //defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+}
+
+EventEx::EventEx(const EventEx& b)
+{
+	m_isManualReset=false;
+	m_lpsaAttributes=NULL;
+	if(b.m_lpsaAttributes)
+	{
+		m_lpsaAttributes=EP_NEW SECURITY_ATTRIBUTES();
+		*m_lpsaAttributes=*b.m_lpsaAttributes;
+	}
+	m_isInitialRaised=b.m_isInitialRaised;
+	m_name=b.m_name;
+	if(m_name.size())
+		m_event=CreateEvent(m_lpsaAttributes,(BOOL)m_isManualReset,(BOOL)m_isInitialRaised,m_name.c_str());
+	else
+		m_event=CreateEvent(m_lpsaAttributes,(BOOL)m_isManualReset,(BOOL)m_isInitialRaised,NULL);
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	m_eventDebug=CreateEvent(m_lpsaAttributes,FALSE,TRUE,NULL);
+	m_threadID=0;
+	if(m_isInitialRaised)
+		m_threadID=GetCurrentThreadId();
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+}
+
+EventEx::~EventEx()
+{
+
+	CloseHandle(m_event);
+	if(m_lpsaAttributes)
+	{
+		EP_DELETE m_lpsaAttributes;
+		m_lpsaAttributes=NULL;
+	}
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	CloseHandle(m_eventDebug);
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+}
+
+EventEx & EventEx::operator=(const EventEx&b)
+{
+	if(this != &b)
+	{
+		CloseHandle(m_event);
+		if(m_lpsaAttributes)
+		{
+			EP_DELETE m_lpsaAttributes;
+			m_lpsaAttributes=NULL;
+		}
+
+		if(b.m_lpsaAttributes)
+		{
+			m_lpsaAttributes=EP_NEW SECURITY_ATTRIBUTES();
+			*m_lpsaAttributes=*b.m_lpsaAttributes;
+		}
+		m_isManualReset=b.m_isManualReset;
+		m_isInitialRaised=b.m_isInitialRaised;
+		m_name=b.m_name;
+		if(m_name.size())
+			m_event=CreateEvent(m_lpsaAttributes,(BOOL)m_isManualReset,(BOOL)m_isInitialRaised,m_name.c_str());
+		else
+			m_event=CreateEvent(m_lpsaAttributes,(BOOL)m_isManualReset,(BOOL)m_isInitialRaised,NULL);
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+		m_threadID=0;
+		if(m_isInitialRaised)
+			m_threadID=GetCurrentThreadId();
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	}
+	return *this;
+}
+bool EventEx::Lock()
+{
+
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	System::WaitForSingleObject(m_eventDebug,WAITTIME_INIFINITE);
+	unsigned long threadID=GetCurrentThreadId();
+	EP_ASSERT_EXPR(threadID!=m_threadID,_T("Possible Deadlock detected!"));
+	::SetEvent(m_eventDebug);
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+
+	bool returnVal=true;
+	unsigned long res=System::WaitForSingleObject(m_event,WAITTIME_INIFINITE);
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	System::WaitForSingleObject(m_eventDebug,WAITTIME_INIFINITE);
+	m_threadID=threadID;
+	::SetEvent(m_eventDebug);
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	return returnVal;
+}
+
+long EventEx::TryLock()
+{
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	System::WaitForSingleObject(m_eventDebug,WAITTIME_INIFINITE);
+	unsigned long threadID=GetCurrentThreadId();
+	EP_ASSERT_EXPR(threadID!=m_threadID,_T("Possible Deadlock detected!"));
+	::SetEvent(m_eventDebug);
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	long ret=0;
+	unsigned long eventStatus=System::WaitForSingleObject(m_event,WAITTIME_IGNORE);
+	if(eventStatus== WAIT_OBJECT_0)
+		ret=1;
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	if(ret)
+	{
+		System::WaitForSingleObject(m_eventDebug,WAITTIME_INIFINITE);
+		m_threadID=threadID;
+		::SetEvent(m_eventDebug);
+	}
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	return ret;
+
+}
+
+long EventEx::TryLockFor(const unsigned int dwMilliSecond)
+{
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	System::WaitForSingleObject(m_eventDebug,WAITTIME_INIFINITE);
+	unsigned long threadID=GetCurrentThreadId();
+	EP_ASSERT_EXPR(threadID!=m_threadID,_T("Possible Deadlock detected!"));
+	::SetEvent(m_eventDebug);
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	long ret=0;
+	unsigned long eventStatus=System::WaitForSingleObject(m_event,dwMilliSecond);
+	if(eventStatus==WAIT_OBJECT_0)
+		ret=1;
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	if(ret)
+	{
+		System::WaitForSingleObject(m_eventDebug,WAITTIME_INIFINITE);
+		m_threadID=threadID;
+		::SetEvent(m_eventDebug);
+	}
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	return ret;
+
+}
+void EventEx::Unlock()
+{
+#if defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	System::WaitForSingleObject(m_eventDebug,WAITTIME_INIFINITE);
+	m_threadID=0;
+	::SetEvent(m_eventDebug);
+#endif // defined(_DEBUG) && defined(ENABLE_POSSIBLE_DEADLOCK_CHECK)
+	::SetEvent(m_event);
+}
+
+bool EventEx::IsManualReset() const
+{
+	return m_isManualReset;
+}
+
+bool EventEx::ResetEvent()
+{
+	return (bool)::ResetEvent(m_event);
+}
+
+bool EventEx::SetEvent()
+{
+	return (bool)::SetEvent(m_event);
+}
+bool EventEx::WaitForEvent(const unsigned int dwMilliSecond)
+{	
+	unsigned long eventStatus=System::WaitForSingleObject(m_event,dwMilliSecond);
+	if(eventStatus==WAIT_OBJECT_0)
+		return true;
+	return false;
+}
