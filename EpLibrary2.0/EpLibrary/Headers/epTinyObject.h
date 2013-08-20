5d5e3f43a51089744efdce7175915dc78d2a6fdb
Woong Gyu La <juhgiyo@gmail.com>
2013-08-20 21:23:32 +0900

    Re-distribution under MIT License

--- EpLibrary2.0/EpLibrary/Headers/epTinyObject.h
+++ EpLibrary2.0/EpLibrary/Headers/epTinyObject.h
@@ -9,6 +9,32 @@
 
 @section LICENSE
 
+The MIT License (MIT)
+
+Copyright (c) 2012-2013 Woong Gyu La <juhgiyo@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+------------------------------------------------------------------------------
+
+Part of this software is
+
 The Loki Library
 Copyright (c) 2001 by Andrei Alexandrescu
 This code accompanies the book:
@@ -20,21 +46,6 @@ Permission to use, copy, modify, distribute and sell this software for any
     permission notice appear in supporting documentation.
 The author or Addison-Wesley Longman make no representations about the 
     suitability of this software for any purpose. It is provided "as is" 
-
-Copyright (C) 2012  Woong Gyu La <juhgiyo@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 @section DESCRIPTION
 
--- EpLibrary2.0/EpLibrary/Sources/epTinyObject.cpp
+++ EpLibrary2.0/EpLibrary/Sources/epTinyObject.cpp
@@ -1,31 +1,44 @@
 /*! 
+TinyObject for the EpLibrary
+
+The MIT License (MIT)
+
+Copyright (c) 2012-2013 Woong Gyu La <juhgiyo@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+
+------------------------------------------------------------------------------
+
+Part of this software is
+
 The Loki Library
 Copyright (c) 2001 by Andrei Alexandrescu
 This code accompanies the book:
 Alexandrescu, Andrei. "Modern C++ Design: Generic Programming and Design 
-    Patterns Applied". Copyright (c) 2001. Addison-Wesley.
+Patterns Applied". Copyright (c) 2001. Addison-Wesley.
 Permission to use, copy, modify, distribute and sell this software for any 
-    purpose is hereby granted without fee, provided that the above copyright 
-    notice appear in all copies and that both that copyright notice and this 
-    permission notice appear in supporting documentation.
+purpose is hereby granted without fee, provided that the above copyright 
+notice appear in all copies and that both that copyright notice and this 
+permission notice appear in supporting documentation.
 The author or Addison-Wesley Longman make no representations about the 
-    suitability of this software for any purpose. It is provided "as is" 
-
-TinyObject for the EpLibrary
-Copyright (C) 2012  Woong Gyu La <juhgiyo@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+suitability of this software for any purpose. It is provided "as is" 
 */
 #include "epTinyObject.h"
 #include "epException.h"
